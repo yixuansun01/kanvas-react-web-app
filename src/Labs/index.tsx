daac099043fc6317400fed9b53df1788906df38e
yixuansun <sun.yixu@northeastern.edu>
2024-03-18 20:15:37 -0700

    a4 update

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -14,6 +14,8 @@ function Labs() {
           <h1>Labs</h1>
           <Nav/>
           <Link to="/Labs/a3">Assignment 3</Link>
+          <br/>
+          <Link to="/Labs/a4">Assignment 4</Link>
           <Routes>
             <Route path="/a3/*" element={<Assignment3 />} />
             <Route path="/a4/*" element={<Assignment4/>}/>
